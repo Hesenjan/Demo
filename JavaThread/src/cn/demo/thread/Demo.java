2cef9124287aae8da4f986bdd2fc61efedc28f91
Hesenjan <Hesenjan@192.168.0.11>
2015-07-08 13:35:14 +0800

    Thread

--- JavaThread/src/cn/demo/thread/Demo.java
+++ JavaThread/src/cn/demo/thread/Demo.java
@@ -2,9 +2,53 @@ package cn.demo.thread;
 
 public class Demo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void main(String[] args) {
+	        A a = new A();
+	        B b = new B();
+	        C c = new C();
+	        a.start();
+	        b.start();
+	        c.start();
+	    }
+
 
 	}
 
-}
+	class A extends Thread {
+	    public A() {
+
+	    }
+
+	    @Override
+	    public void run() {
+	        for (int i = 0; i < 10; i++) {
+	            System.out.println("A" + i);
+	        }
+	    }
+	}
+
+	class B extends Thread {
+	    public B() {
+
+	    }
+
+	    @Override
+	    public void run() {
+	        for (int i = 0; i < 10; i++) {
+	            System.out.println("B" + i);
+	        }
+	    }
+	}
+
+	class C extends Thread {
+	    public C() {
+
+	    }
+
+	    @Override
+	    public void run() {
+	        for (int i = 0; i < 10; i++) {
+	            System.out.println("C" + i);
+	        }
+	    }
+	}
